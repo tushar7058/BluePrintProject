a2201c90ba0a00e4decf6d6dcbb38b686a52baa0
MayurDeshmukh96 <mayurdeshmukh8570@gmail.com>
2025-04-17 18:05:02 +0530

    sample database created

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Creator
+
+admin.site.register(Creator)
+
 # Register your models here.
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    number = models.IntegerField()
+
